603513992dd99f96f1ead35272831c1555e27f28
Afia Rahman <rahmanmou26@gmail.com>
2024-03-13 13:29:47 +0600

    Shown more data of user

--- src/User/User.jsx
+++ src/User/User.jsx
@@ -5,6 +5,12 @@ export default function User({user}){
         <div className="user-container">
             <h2>{user.name}</h2>
             <p>{user.email}</p>
+            <h3>Address</h3>
+            <div className="address-container">
+                <p>{user.address.street}</p>
+                <p>{user.address.city}</p>
+                <p>{user.address.zipcode}</p>
+            </div>
         </div>
     )
 }
